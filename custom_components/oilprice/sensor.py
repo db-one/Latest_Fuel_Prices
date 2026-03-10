bbc9bc30063e51ef8817ecb767790913ba25a1a7
db-one. <20243226+db-one@users.noreply.github.com>
2026-03-10 08:58:25 +0800

    Fix syntax errors and update comments in sensor.py

--- custom_components/oilprice/sensor.py
+++ custom_components/oilprice/sensor.py
@@ -19,21 +19,21 @@ from bs4 import BeautifulSoup
 __version__ = '0.1.0'
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['requests', 'beautifulsoup4']
+REQUIREMENTS = ['requests'， 'beautifulsoup4']
 
 COMPONENT_REPO = 'https://github.com/aalavender/OilPrice/'
-SCAN_INTERVAL = datetime.timedelta(hours=1)
+SCAN_INTERVAL = datetime.timedelta(hours=1) # 更新间隔时间
 ICON = 'mdi:gas-station'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_NAME): cv.string,
-    vol.Required(CONF_REGION): cv.string,
+    vol.必要的(CONF_NAME): cv.string，
+    vol.必要的(CONF_REGION): cv.string，
 })
 
 
 async def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     _LOGGER.info("async_setup_platform sensor oilprice")
-    async_add_devices([OilPriceSensor(name=config[CONF_NAME], region=config[CONF_REGION])],True)
+    async_add_devices([OilPriceSensor(name=config[CONF_NAME], region=config[CONF_REGION])]，True)
 
 
 class OilPriceSensor(Entity):
@@ -48,17 +48,17 @@ class OilPriceSensor(Entity):
         header = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36'
         }
-        response = request('GET', 'http://www.qiyoujiage.com/' + self._region + '.shtml', headers=header)  # 定义头信息发送请求返回response对象
+        response = request('GET'， 'http://www.qiyoujiage.com/' + self._region + '.shtml', headers=header)  # 定义头信息发送请求返回response对象
         response.encoding = 'utf-8'   #不写这句会乱码
-        soup = BeautifulSoup(response.text, "lxml")
+        soup = BeautifulSoup(response.text， "lxml")
         dls = soup.select("#youjia > dl")
-        self._state = soup.select("#youjiaCont > div")[1].contents[0].strip()
+        self._state = soup.select("#youjiaCont > div")[1]。contents[0]。strip()
 
         for dl in dls:
-            k = re.search("\d+", dl.select('dt')[0].text).group()
-            self._entries[k] = dl.select('dd')[0].text
-        self._entries["update_time"] = datetime.datetime.now().strftime('%Y-%m-%d')
-        self._entries["tips"] = soup.select("#youjiaCont > div:nth-of-type(2) > span")[0].text.strip()  # 油价涨跌信息
+            k = re.search("\d+", dl.select('dt')[0]。text)。group()
+            self._entries[k] = dl.select('dd')[0]。text
+        self._entries["update_time"] = datetime.datetime。当前()。strftime('%Y-%m-%d')
+        self._entries["tips"] = soup.select("#youjiaCont > div:nth-of-type(2) > span")[0]。text。strip()  # 油价涨跌信息
 
     @property
     def name(self):
